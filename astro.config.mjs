247511391130a6f682293256750e4b0b378e7368
jinrd <gkwlsdnjs95@gmail.com>
2025-12-29 09:51:03 +0900

    change server mode node -> vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+// import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite'; // <-- 여기가 핵심 변경점!
 
@@ -8,9 +9,9 @@ import svelte from '@astrojs/svelte';
 
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
+  //   mode: 'standalone',
+  // }),
   // integrations 배열에서 tailwind()를 뺐습니다. (v4는 여기서 안 씀)
   integrations: [react(), svelte()],
   // vite 설정 안에 플러그인으로 넣습니다.
